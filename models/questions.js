b9b631508135663fcd2fed2128bd65ac83fe6fde
slyder219 <seanlyder@gmail.com>
2023-06-04 20:40:55 -0400

    syncing

--- models/questions.js
+++ models/questions.js
@@ -1,9 +1,9 @@
-const questions = [
-    'How adventurous are you?',
-    'How often do you like to try new things?',
-    'How interested are you in learning about local cultures?',
-    'How important is it to you to have a plan when you travel?',
-    'Beachy or mountainous? 1 for beachy, 5 for mountainous, 3 for somwhere in the middle.'
-]
+const questions = {
+    'How adventurous are you?': ['Very', 'Somewhat', 'Not Very', 'Not at all'],
+    'How often do you like to try new things?': ['Whenever I can', 'Sometimes', 'Rarely', 'Never'],
+    'How interested are you in learning about local cultures?': ['Very', 'Somewhat', 'Not Very', 'Not at all'],
+    'How important is it to you to have a plan when you travel?': ['Very', 'Somewhat', 'Not Very', 'Not at all'],
+    'Beachy or mountainous?': ['Beachy!', 'Mountainous!', 'I like both!']
+  };
 
 module.exports = questions;
--- public/scripts/welcomeQuizScript.js
+++ public/scripts/welcomeQuizScript.js
@@ -1,3 +1,11 @@
+const questions = {
+    'How adventurous are you?': ['Very', 'Somewhat', 'Not Very', 'Not at all'],
+    'How often do you like to try new things?': ['Whenever I can', 'Sometimes', 'Rarely', 'Never'],
+    'How interested are you in learning about local cultures?': ['Very', 'Somewhat', 'Not Very', 'Not at all'],
+    'How important is it to you to have a plan when you travel?': ['Very', 'Somewhat', 'Not Very', 'Not at all'],
+    'Beachy or mountainous?': ['Beachy!', 'Mountainous!', 'I like both!']
+  };
+  
 const form = document.getElementById("personalityForm");
 const submitButton = document.getElementById("submitPersonality");
 
